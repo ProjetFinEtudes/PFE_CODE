3f00dbca7ae2404e388463482025aa3659042d16
mounacqflo <mounacqflo@cy-tech.fr>
2023-06-18 17:02:44 +0200

    retrieve user and fill inputs in the form

--- chatbot-frontend/src/app/pages/profile/profile.component.ts
+++ chatbot-frontend/src/app/pages/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { User } from 'src/app/interfaces/user';
 import { Password } from 'src/app/interfaces/password';
+import { UserService } from 'src/app/services/user.service';
 
 
 @Component({
@@ -13,6 +14,21 @@ export class ProfileComponent {
 
   user: User = <User>{};
   password: Password = <Password>{};
+
+  constructor(public userService: UserService) { 
+    this.user = this.userService.user;
+  }
+
+  updateUser() {
+    console.log(this.user);
+    // this.userService.updateUser(this.user)
+    //   .subscribe(
+    //     (user: User) => {
+    //       this.user = user;
+    //       console.log(this.user);
+    //     }
+    //   );
+  }
 
   changePassword() {
     console.log(this.password);
